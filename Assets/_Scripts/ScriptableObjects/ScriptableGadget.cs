284b4e3659f1cd7ff486982bdfd2057e00f375bc
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-01-19 23:39:29 -0500

    added ScriptableGadget.cs

---
+++ Assets/_Scripts/ScriptableObjects/ScriptableGadget.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Gadgets", menuName = "ScriptableObjects/Gadgets")]
+public class ScriptableGadget : ScriptableObject
+{
+    public GadgetType gadgetType;
+    public LayerMask playerLayer;
+
+    [SerializeField] private Stats _advancedStats;
+    public Stats avancedStats => _advancedStats;
+
+    [System.Serializable]
+    public enum GadgetType {
+        Umbrella,
+        Tire,
+        Rocket,
+        Wheel,
+    }
+
+    /// <summary>
+    /// Keeping base stats as a struct on the scriptable keeps it flexible and easily editable.
+    /// We can pass this struct to the spawned prefab unit and alter them depending on conditions.
+    /// </summary>
+    [System.Serializable]
+    public struct Stats
+    {
+        public float mass;
+        public bool canFly;
+        public float airDrag;
+        public bool isForBoost;
+        public bool isForJump;
+        public float jumpForce;
+    }
+}

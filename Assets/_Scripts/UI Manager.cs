3f42ace2650335287f9d85566a8e54900efe67e8
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-01-19 22:57:44 -0500

    create ui manager and game manager

--- Assets/_Scripts/UI Manager.cs
+++ Assets/_Scripts/UI Manager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    GameObject containers;
+    GameManager gameManager;
+    public int coins;
     // Start is called before the first frame update
     void Start()
-    {
+    {        
+        containers = GameObject.Find("Containers");
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         
     }
 
@@ -14,5 +20,15 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void LevelComplete()
+    {
+        gameManager.LevelComplete();
+    }
+    public void GameOverSequence()
+    {
+        gameManager.GameOver();
+        // StartCoroutine(GameOverRoutine());
+
     }
 }
